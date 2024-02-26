7d0b96aed7f8d560ec3b0fb5c1d36deea7ff7457
Mekdlsdl <ekdlsdl0@hufs.ac.kr>
2024-02-26 20:31:40 +0900

    0221 hw 별 찍기

---
+++ Homework/20240221_hw/main01.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    printf("1. 별찍기 4종\n");
+    printf("- 1종\n");
+
+    for (int i = 1; i < 6; i++) {
+        for (int j = 0; j < i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    printf("\n- 2종\n");
+
+    for (int i = 1; i < 6; i++) {
+        for (int j = 0; j < 6 - i; j++) {
+            printf(" ");
+        }
+
+        for (int k = 0; k < i; k++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    printf("\n- 3종\n");
+
+    for (int i = 1; i < 6; i++) {
+        for (int j = 0; j < 6 - i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    printf("\n- 4종\n");
+
+    for (int i = 1; i < 6; i++) {
+        for (int j = 0; j < i; j++) {
+            printf(" ");
+        }
+
+        for (int k = 0; k < 6 - i; k++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
